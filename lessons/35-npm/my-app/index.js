2d11bf58b716524e9cb543b2fe0fc06ce6dabbdb
Darya <darya.aniskevich@mail.ru>
2021-04-18 19:30:34 +0300

    lesson 35 npm

--- lessons/35-npm/my-app/index.js
+++ lessons/35-npm/my-app/index.js
@@ -1,4 +1,5 @@
 // import functions from your package
-const { smartSearch } = require('[NAME_OF_YOUR_PACKAGE]')
+const { smartSearch, randomSearch } = require('darya-les-35-npm-first-package');
 
-console.log(smartSearch(['Smith', 'John'], 'John'))
+console.log(smartSearch(['Smith', 'John'], 'John'));
+console.log(randomSearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
