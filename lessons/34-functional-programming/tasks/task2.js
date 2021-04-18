88adee5e1602b2a169d32f7c16a94d8517188124
Darya <darya.aniskevich@mail.ru>
2021-04-18 23:13:35 +0300

    less 24 FP

--- lessons/34-functional-programming/tasks/task2.js
+++ lessons/34-functional-programming/tasks/task2.js
@@ -1,4 +1,26 @@
-const memoizeResultCreator = () => {};
+const memoizeResultCreator = (...funcs) => {
+  const paramFuncs = funcs.slice(0, funcs.length - 1);
+  const resultFunc = funcs[funcs.length - 1];
+  const cache = [];
+
+  let isNeedToChange = false;
+  let res;
+  return function (value) {
+    const result = paramFuncs.map((paramFn, index) => {
+      if (paramFn(value) !== cache[index]) {
+        isNeedToChange = true;
+      }
+      return paramFn(value);
+    });
+
+    if (isNeedToChange) {
+      res = resultFunc(...result);
+      cache.push(...result);
+      isNeedToChange = false;
+    }
+    return res;
+  };
+};
 
 module.exports = {
   memoizeResultCreator,
